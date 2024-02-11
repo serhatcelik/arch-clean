521f9ca4759a3e2880d35da19254a159d2b124bc
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2024-02-12 01:11:37 +0300

    Trying to fix

--- README.md
+++ README.md
@@ -4,16 +4,12 @@ Clone this repo as `root` user:
 git clone https://github.com/serhatcelik/arch-clean.git
 ```
 
-After that run the following commands:
+Then run the commands below and follow the instructions:
 
 ```bash
 cd arch-clean
 chmod +x install.sh
 ./install.sh
-cd ..
-rm -rf arch-clean
 ```
-
-Reboot and log in as new user.
 
 **Tested under Arch Linux 2024.02.01**
--- install.sh
+++ install.sh
@@ -1,73 +1,87 @@
 #!/bin/bash
 
-###################
-# Get credentials #
-###################
-read -r -p "Enter username: " USER
-read -r -s -p "Enter password: " PASS
-
-[ -z "$USER" ] || [ -z "$PASS" ] && echo -e "\nNo credentials.\nExit." && exit 1
-
-HOME_DIR="/home/$USER"
-
-###############
-# Create user #
-###############
-useradd --gid users --groups wheel --create-home "$USER" --home-dir "$HOME_DIR"
-yes "$PASS" | passwd "$USER"
-echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
-
-####################
-# Install packages #
-####################
-pacman -Syu --noconfirm alacritty alsa-utils arandr base-devel bluez bluez-utils caja curl dkms feh ffmpeg firefox flameshot gcc git htop i3 iptables ipython john jq libreoffice make man mesa neofetch net-tools netcat networkmanager openssh openvpn p7zip picom powerline powerline-fonts pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer python-pip rofi sudo unzip vim virtualbox-guest-utils vulkan-icd-loader wget which wireshark-cli wireshark-qt xf86-input-evdev xf86-input-libinput xf86-input-synaptics xf86-input-vmmouse xorg xorg-xinit zsh
-
-################
-# Change shell #
-################
-chsh "$USER" --shell "$(which zsh)"
-
-################
-# Autostarting #
-################
-systemctl enable bluetooth.service
-systemctl enable NetworkManager.service
-systemctl enable systemd-timesyncd.service
-systemctl enable fstrim.timer
-
-########################
-# Install Sublime Text #
-########################
-curl -O https://download.sublimetext.com/sublimehq-pub.gpg && pacman-key --add sublimehq-pub.gpg && pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
-if grep -q "\[sublime-text\]" /etc/pacman.conf
+if [ "$(id -u)" -eq 0 ]
 then
-    :
-else
-    echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | tee -a /etc/pacman.conf
+    echo "YOU ARE ROOT."
 fi
-pacman -Syu --noconfirm sublime-text
+#!/bin/bash
 
-#####################
-# Install Powerline #
-#####################
-git clone https://github.com/powerline/fonts.git --depth=1
-cd fonts || exit 1
-sudo -u "$USER" chmod +x install.sh
-sudo -u "$USER" ./install.sh
-cd ..
-rm -rf fonts
+prepare() {
+    read -r -p "Enter username: " USER
+    read -r -s -p "Enter password: " PASS
 
-#####################
-# Install Oh My Zsh #
-#####################
-sudo -u "$USER" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-sudo -u "$USER" git clone https://github.com/zsh-users/zsh-autosuggestions.git "$HOME_DIR"/.oh-my-zsh/plugins/zsh-autosuggestions
-sudo -u "$USER" git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME_DIR"/.oh-my-zsh/plugins/zsh-syntax-highlighting
+    if id -u "$USER"
+    then
+        return 1
+    fi
 
-##############
-# Copy files #
-##############
-sudo -u "$USER" cp -a my/. "$HOME_DIR"
+    if [ -z "$USER" ] || [ -z "$PASS" ]
+    then
+        return 1
+    fi
 
-echo "Done."
-echo "Reboot and log in as $USER."
+    HOME_DIR="/home/$USER"
+
+    if ! useradd --gid users --groups wheel --shell "$(which zsh)" --create-home "$USER" --home-dir "$HOME_DIR"
+    then
+        return 1
+    fi
+
+    yes "$PASS" | passwd "$USER"
+
+    EDITOR=$(which vim)
+    export EDITOR
+    visudo
+
+    if ! pacman -Syu --noconfirm alacritty alsa-utils arandr base-devel bluez bluez-utils caja curl dkms feh ffmpeg firefox flameshot gcc git htop i3 iptables ipython john jq libreoffice make man mesa neofetch net-tools netcat networkmanager openssh openvpn p7zip picom powerline powerline-fonts pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer python-pip rofi sudo unzip vim virtualbox-guest-utils vulkan-icd-loader wget which wireshark-cli wireshark-qt xf86-input-evdev xf86-input-libinput xf86-input-synaptics xf86-input-vmmouse xorg xorg-xinit zsh
+    then
+        return 1
+    fi
+
+    systemctl enable bluetooth.service
+    systemctl enable iwd.service
+    systemctl enable NetworkManager.service
+    systemctl enable systemd-timesyncd.service
+    systemctl enable fstrim.timer
+
+    if [ $? -ne 0 ]
+    then
+        return 1
+    fi
+
+    echo "DONE."
+    echo "Reboot and log in as $USER."
+    echo "Then run me again."
+}
+
+install() {
+    if ! grep -q "\[sublime-text\]" /etc/pacman.conf
+    then
+        curl -O https://download.sublimetext.com/sublimehq-pub.gpg && pacman-key --add sublimehq-pub.gpg && pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
+        echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | tee -a /etc/pacman.conf
+        sudo pacman -Syu --noconfirm sublime-text
+    fi
+
+    git clone https://github.com/powerline/fonts.git --depth=1
+    cd fonts || exit 1
+    chmod +x install.sh
+    ./install.sh
+    cd ..
+    rm -rf fonts
+
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+
+    cp -a my/. ~
+
+    echo "DONE."
+    echo "Log out and log back in again."
+}
+
+if [ "$(id -u)" -eq 0 ]
+then
+    prepare
+else
+    install
+fi
