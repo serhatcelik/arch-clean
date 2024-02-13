9273deeb46ac38fc7f321e4e4dfbbcc8947571f9
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2024-02-13 23:22:01 +0300

    Small changes

--- my/.config/polybar/launch.sh
+++ my/.config/polybar/launch.sh
@@ -6,7 +6,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 polybar primary &
 
-if xrandr -q | grep -q "HDMI.* connected"
+if xrandr --query | grep -q "HDMI.* connected"
 then
     polybar external &
 fi
