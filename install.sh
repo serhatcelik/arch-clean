e142104d13144df0af99e9d43a50dd0305ca0d1d
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2024-02-13 21:27:16 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ fi
 if ! grep -q "\[sublime-text\]" /etc/pacman.conf
 then
     curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
-    echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | tee -a /etc/pacman.conf
+    echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
     sudo pacman -Syu sublime-text --noconfirm --needed
 fi
 
