2cbc7411dd46df088a9c2fcc10889c1ddf085512
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2024-02-22 08:22:01 +0300

    Back again

--- my/.config/polybar/launch.sh
+++ my/.config/polybar/launch.sh
@@ -4,11 +4,6 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-if ! [ -e /sys/class/backlight/intel_backlight ]
-then
-    sed -i "s/hidden = false/hidden = true/" ~/.config/polybar/config.ini
-fi
-
 polybar primary &
 
 if xrandr --query | grep -q "HDMI.* connected"
