62db6f3d744916f2a11456b64a217d61e8e84fd8
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2024-03-11 16:24:21 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -19,8 +19,7 @@ fi
 
 # Install Sublime Text
 curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
-echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
-sudo awk -i inplace '!seen[$0]++' /etc/pacman.conf
+grep -q "sublime-text" /etc/pacman.conf || echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
 sudo pacman -Syu sublime-text --noconfirm --needed
 
 # Install Oh My Zsh
