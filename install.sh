bc3e8961af62996f4c5b9c4e37cb47cd5bd5c336
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2024-03-16 00:34:47 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ else
 fi
 
 # Install packages
-if ! sudo pacman -Syu --noconfirm --needed alacritty alsa-utils arandr base base-devel bind bluez bluez-utils brightnessctl caja copyq coreutils curl dkms feh ffmpeg firefox flameshot gcc git github-cli htop i3 intel-media-driver iptables ipython john jq libreoffice libva-intel-driver libva-mesa-driver lightdm lightdm-gtk-greeter make man mesa nano neofetch net-tools netcat networkmanager nmap ntfs-3g openssh openvpn p7zip papirus-icon-theme pavucontrol picom polybar powerline powerline-fonts pulseaudio python python-pip reflector rofi sudo tk unzip vim vulkan-intel vulkan-radeon wget which wireshark-cli wireshark-qt xf86-input-libinput xf86-video-amdgpu xf86-video-ati xf86-video-nouveau xf86-video-vmware xorg xorg-server xorg-xinit xss-lock zip zsh
+if ! sudo pacman -Syu --noconfirm --needed alacritty alsa-utils arandr base base-devel bind bluez bluez-utils brightnessctl caja copyq coreutils curl dkms feh ffmpeg firefox flameshot gcc git github-cli htop i3 inetutils intel-media-driver iptables ipython john jq libreoffice libva-intel-driver libva-mesa-driver lightdm lightdm-gtk-greeter make man mesa nano neofetch net-tools netcat networkmanager nmap ntfs-3g openssh openvpn p7zip papirus-icon-theme pavucontrol picom polybar powerline powerline-fonts pulseaudio python python-pip reflector rofi sudo tk unzip vim vulkan-intel vulkan-radeon wget which wireshark-cli wireshark-qt xf86-input-libinput xf86-video-amdgpu xf86-video-ati xf86-video-nouveau xf86-video-vmware xorg xorg-server xorg-xinit xss-lock zip zsh
 then
     echo "Alas, Pacman failed."
     exit 1
