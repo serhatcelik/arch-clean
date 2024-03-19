195c13e2cb87c54c2f00f1a7c1014e558a8e8fe9
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2024-03-19 09:55:49 +0300

    Add notifications

--- my/.local/bin/power.py
+++ my/.local/bin/power.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
-
 import sys
 import tkinter.messagebox as mb
-
-
 def ask():
     if not mb.askyesno(sys.argv[1], 'Are you sure?', default='no'):
         sys.exit(1)
-
-
 ask()
--- my/.local/bin/volume.sh
+++ my/.local/bin/volume.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-notify-send --urgency low --hint=string:x-dunst-stack-tag:volume "Volume" "$(pactl get-sink-$1 @DEFAULT_SINK@)"
+get() {
+    pactl get-sink-volume @DEFAULT_SINK@ | grep -o -E "[0-9]+%"
+}
+if [ "$1" = "raise" ]
+then
+    if ! grep -q "15[0-9]%" <<< "$(get)"
+    then
+        pactl set-sink-volume @DEFAULT_SINK@ +5%
+    fi
+    notify-send --urgency low --hint=string:x-dunst-stack-tag:volume "Volume" "$(get)"
+elif [ "$1" = "lower" ]
+then
+    pactl set-sink-volume @DEFAULT_SINK@ -5%
+    notify-send --urgency low --hint=string:x-dunst-stack-tag:volume "Volume" "$(get)"
+elif [ "$1" = "mute" ]
+then
+    pactl set-sink-mute @DEFAULT_SINK@ toggle
+    notify-send --urgency low --hint=string:x-dunst-stack-tag:volume "Volume" "$(pactl get-sink-mute @DEFAULT_SINK@)"
+fi
