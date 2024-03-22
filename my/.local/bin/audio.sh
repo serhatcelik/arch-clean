18e99b2bf08efce8432721dfe1f9a3aa8273f747
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2024-03-22 08:01:26 +0300

    Update configs

--- my/.local/bin/audio.sh
+++ my/.local/bin/audio.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-pulseaudio-ctl "$1" "$2"
 notify-send --hint=string:x-dunst-stack-tag:my "Volume" "$(pulseaudio-ctl full-status | cut -d " " -f -2 | sed 's/yes/(mute)/;s/no//')"
--- my/.local/bin/backlight.sh
+++ my/.local/bin/backlight.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-BRIGHTNESS="$(brightnessctl set "$1" 2> /dev/null | grep -o -P "[0-9]+(?=%)")"
-[ -n "$2" ] || notify-send --hint=string:x-dunst-stack-tag:my "Brightness" "$BRIGHTNESS"
-echo "$BRIGHTNESS"
+notify-send --hint=string:x-dunst-stack-tag:my "Brightness" ""$(brightnessctl set +0 | grep -o -P "[0-9]+(?=%)")""
