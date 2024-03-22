dea023272f8fe920f70cd434d64da25350a8e648
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2024-03-22 16:40:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,11 +15,6 @@ then
     exit 1
 fi
 
-# Install pulseaudio-ctl
-git clone https://aur.archlinux.org/pulseaudio-ctl.git
-makepkg -is --noconfirm --needed -D pulseaudio-ctl
-rm -rf pulseaudio-ctl
-
 # Install Sublime Text
 curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
 grep -q "sublime-text" /etc/pacman.conf || echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
