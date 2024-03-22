e0e5ad125052a99b1928ad8fce4184859872055c
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2024-03-22 07:09:16 +0300

    Update configs

--- my/.local/bin/backlight.sh
+++ my/.local/bin/backlight.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 BRIGHTNESS="$(brightnessctl set "$1" 2> /dev/null | grep -o -P "[0-9]+(?=%)")"
-notify-send --hint=string:x-dunst-stack-tag:my "Brightness" "$BRIGHTNESS"
+[ -n "$2" ] || notify-send --hint=string:x-dunst-stack-tag:my "Brightness" "$BRIGHTNESS"
 echo "$BRIGHTNESS"
