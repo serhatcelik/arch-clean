b4d46552c800d709e2cc7b16eccc281e683c0559
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2024-03-24 18:49:39 +0300

    Update notify.sh

--- my/.local/bin/notify.sh
+++ my/.local/bin/notify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 Audio() {
-    pactl --format json list sinks | sed -n -E 's/.*"mute":(false|true).*"value_percent":"(.+)%.*,"front-right":.*/\2\1/p' | sed 's/false//;s/true/(mute)/'
+    pactl --format json list sinks | sed -n -E 's/.*,"mute":(false|true),.*,"value_percent":"([0-9]+)%",.*,"front-right":.*/\2\1/p' | sed 's/false//;s/true/(mute)/'
 }
 Brightness() {
     brightnessctl set +0 | grep -o -P "[0-9]+(?=%)"
