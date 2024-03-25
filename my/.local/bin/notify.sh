8e4365133ac8772ff0792e9c62fc937a9c2e413c
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2024-03-25 15:06:57 +0300

    Update configs

--- my/.local/bin/notify.sh
+++ my/.local/bin/notify.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 Audio() {
+    polybar-msg action audio "$1" > /dev/null 2>&1
     pactl --format json list sinks | sed -n -E 's/.*,"mute":(false|true),.*,"value_percent":"([0-9]+)%",.*,"front-right":.*/\2\1/p' | sed 's/false//;s/true/(mute)/'
 }
 Brightness() {
+    polybar-msg action backlight "$1" > /dev/null 2>&1
     brightnessctl set +0 | grep -o -P "[0-9]+(?=%)"
 }
-notify-send --hint=string:x-dunst-stack-tag:my "$1" "$($1)"
+Touchpad() {
+    ID="$(xinput list | sed -n -E 's/.*Touchpad.*id=([0-9]+).*/\1/p')"
+    xinput --"$1" "$ID"
+    xinput list-props "$ID" | sed -n -E 's/.*Device Enabled.*:.*(0|1).*/\1/p' | sed 's/0/Disabled/;s/1/Enabled/'
+}
+notify-send --hint=string:x-dunst-stack-tag:my "$1" "$($1 "$2")"
