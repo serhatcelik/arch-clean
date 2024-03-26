de0907c7ae26c431f0a00c5352256dbcfadfcb72
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2024-03-26 16:24:23 +0300

    Update configs

--- my/.local/bin/polybar.sh
+++ my/.local/bin/polybar.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
-polybar primary --config="$HOME"/.config/polybar/config.ini &
-if xrandr --query | grep -q "HDMI.* connected"
-then
-    polybar external --config="$HOME"/.config/polybar/config.ini &
-fi
+polybar main --config="$HOME"/.config/polybar/config.ini &
+polybar hdmi --config="$HOME"/.config/polybar/config.ini &
