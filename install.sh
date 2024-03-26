e30c323d2e8ba87800d6cda99db8c75414a0b6d7
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2024-03-26 16:01:28 +0300

    Update install script

--- install.sh
+++ install.sh
@@ -18,7 +18,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 # Download Oh My Zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions.git "$HOME"/.oh-my-zsh/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME"/.oh-my-zsh/plugins/zsh-syntax-highlighting
 
 # Download Powerlevel10k theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
